384f55b6f1a15d315de8ed8c2cbe8f541fd02304
Valentin Kaelin <vkaelin@hey.com>
2022-03-21 16:57:18 +0100

    feat(labo4): ex 3

--- labos/labo4/liste.hs
+++ labos/labo4/liste.hs
@@ -2,13 +2,52 @@
 -- Exercice 3
 -- ---------------------------
 
+module Liste where
+
 -- Définissez un type Liste que vous exposerez via un module avec les fonctionnalités suivantes :
 
--- ■ Définissez les opérations d’insertion et suppression sur une liste triée.
--- ■ Construisez une liste à l’aide d’un pliage et une liste standard.
--- ■ Dérivez le type Liste de Show en implémentant une fonction show plus lisible que la version automatique de Haskell.
--- ■ Implémentez une fonction de recherche qui retourne la liste (de type Liste) des éléments satisfaisant un prédicat.
+data Liste a
+  = Nil
+  | Cons a (Liste a)
 
--- Indication : Il ne faut pas dériver votre type Liste de Show dans la déclaration data sinon il y a
--- un conflit. Ne cherchez pas nécessairement à obtenir une représentation identique à celle des listes
--- standard de Haskell ([1,2,3]).
+testListe :: Liste Integer
+testListe = Cons 1 (Cons 3 (Cons 9 Nil))
+
+-- ■ Définissez les opérations d’insertion et suppression sur une liste triée.
+insertSorted :: Ord a => a -> Liste a -> Liste a
+insertSorted e (Cons h t)
+  | e > h = Cons h $ insertSorted e t
+  | otherwise = Cons e $ Cons h t
+insertSorted e Nil = Cons e Nil
+
+deleteFromList :: Eq t => t -> Liste t -> Liste t
+deleteFromList e (Cons h t)
+  | e == h = t
+  | otherwise = Cons h $ deleteFromList e t
+deleteFromList e Nil = Nil
+
+-- ■ Construisez une liste à l’aide d’un pliage et une liste standard.
+fromList :: [a] -> Liste a
+-- fromList list = foldr (\x acc -> Cons x acc) Nil list
+fromList = foldr Cons Nil
+
+-- ■ Dérivez le type Liste de Show en implémentant une fonction show plus lisible que la version automatique de Haskell.
+instance (Show a) => Show (Liste a) where
+  -- show Nil = []
+  -- show (Cons x xs) = show x ++ " " ++ show xs
+  show Nil = "[]"
+  show (Cons h Nil) = "[" ++ show h ++ "]"
+  show (Cons h hs) = "[" ++ show h ++ show' hs
+    where
+      show' (Cons h hs) = "," ++ show h ++ show' hs
+      show' Nil = "]"
+
+-- ■ Implémentez une fonction de recherche qui retourne la liste (de type Liste)
+-- des éléments satisfaisant un prédicat.
+filterList :: (a -> Bool) -> Liste a -> Liste a
+filterList f (Cons h t)
+  | f h = Cons h $ filterList f t
+  | otherwise = filterList f t
+filterList _ Nil = Nil
+
+-- pour tester: filterList (>8) testListe
