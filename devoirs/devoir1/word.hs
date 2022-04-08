479a469bb76158a279b9ccae1a21e279a5f90022
Jonathan <chil.tfm@gmail.com>
2022-04-08 16:31:07 +0200

    feat: almost working

--- devoirs/devoir1/word.hs
+++ devoirs/devoir1/word.hs
@@ -1,21 +1,40 @@
 import System.Environment
-import Text.Printf
-import Data.Text
 import System.IO
+import Text.Printf
+
 main =
   do
     args <- getArgs
-    progName <- getProgName 
-    if Prelude.null args
-      then usage progName
-      else 
-          putStrLn "Yo"
-          
+    if null args
+      then do
+        progName <- getProgName
+        usage progName
+      else do
+        showTitleLine "file" "words" "lines" "bytes"
+        getNumber (0, 0, 0) args
 
-usage name = 
-    do
-        putStr "usage: ./"
-        putStr name
-        putStrLn " <file1> <file2>"
-        putStrLn "Il faut au minimum 1 fichier"
+getNumber (a, b, c) [] = showLine "total" (a, b, c)
+getNumber (a, b, c) (x : xs) = do
+  handle <- openFile x ReadMode
+  contents <- hGetContents handle
+  let current = countNbWords contents
+  showLine x current
+  getNumber (addTupleOfThree (a, b, c) current) xs
 
+showLine name (a, b, c) = printf "%-10s %-6d %-6d %-6d\n" name a b c
+showTitleLine a b c d = printf "%-10s %6s %6s %6s\n" a b c d
+
+addTupleOfThree (a, b, c) (d, e, f) = (a + d, b + e, c + f)
+
+countNbWords file = (nbWords, nbLines, nbChars)
+  where
+    nbWords = length (words file)
+    nbLines = length (lines file)
+    nbChars = length file
+
+usage name =
+  do
+    putStr "usage: ./"
+    putStr name
+    putStrLn " <file1> <file2> ..."
+    putStrLn "Il faut au minimum 1 fichier"
